a4d5f90e7cd09336e85d82fe4e87e2d1389f309a
miok <mosong@the-51.com>
2023-06-20 15:31:10 +0900

    initial

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/vue-blog/dist/",
+  assetsDir: './',
   outputDir: "docs",
 })
